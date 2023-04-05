aea44b17eb2bcca31ce4bce04ab528f40e734b5d
Harsh3305 <harshverma3305@gmail.com>
2023-04-05 09:37:45 +0530

    First Commit

---
+++ src/main/kotlin/com/hrv/mart/cartresponse/CartResponseApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.cartresponse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CartResponseApplication
+
+fun main(args: Array<String>) {
+	runApplication<CartResponseApplication>(*args)
+}
---
+++ src/test/kotlin/com/hrv/mart/cartresponse/CartResponseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hrv.mart.cartresponse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CartResponseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
